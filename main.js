f35fddcf75bc83f450504138ee8f04073b76f94f
HK Nelson <hknelson8@gmail.com>
2022-02-14 22:29:57 -0500

    add handlebars, template, minor css styling

---
+++ main.js
@@ -0,0 +1,63 @@
+var books = [
+  {
+    title: 'Harry Potter',
+    author: 'J.K. Rowling',
+    imageURL:
+      'https://books.google.com/books/content?id=WV8pZj_oNBwC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api',
+    isbn: '9781921479311',
+    pageCount: 268,
+  },
+];
+
+var renderBooks = function () {
+  $('.books').empty();
+
+  for (var i = 0; i < books.length; i++) {
+    var source = $('#book-template').html();
+    var template = Handlebars.compile(source);
+    var newHTML = template(books[i]);
+
+    $('.books').append(newHTML);
+  }
+};
+
+var addBooks = function (data) {
+  books = [];
+
+  for (var i = 0; i < data.items.length; i++) {
+    var book = {
+      title: data.items[i].volumeInfo.title || null,
+      author: data.items[i].volumeInfo.authors
+        ? data.items[i].volumeInfo.authors[0]
+        : null,
+      imageURL: data.items[i].volumeInfo.imageLinks
+        ? data.items[i].volumeInfo.imageLinks.thumbnail
+        : null,
+      isbn: data.items[i].volumeInfo.industryIdentifiers
+        ? data.items[i].volumeInfo.industryIdentifiers[0].identifier
+        : null,
+      pageCount: data.items[i].volumeInfo.pageCount || null,
+    };
+    books.push(book);
+  }
+  renderBooks();
+};
+
+var fetch = function (query) {
+  $.ajax({
+    method: 'GET',
+    url: 'https://www.googleapis.com/books/v1/volumes?q=' + query,
+    dataType: 'json',
+    success: function (data) {
+      addBooks(data);
+    },
+    error: function (jqXHR, textStatus, errorThrown) {
+      console.log(textStatus);
+    },
+  });
+};
+
+$('.search').on('click', function () {
+  var searchValue = $('#search-query').val();
+  fetch(searchValue);
+});
